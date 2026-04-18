846f8fede2c4918f8ed3e09aa8af26dc740ef3e0
NOUMBI <noumbibe@3il.fr>
2026-04-18 18:09:17 +0200

    fix(backend): correct package declaration and add SpringBoot annotation

---
+++ backend/src/main/java/com/adv/AdvApplication.java
@@ -0,0 +1,11 @@
+package com.adv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdvApplication.class, args);
+    }
+}
